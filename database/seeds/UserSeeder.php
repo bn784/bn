a7c19b5beb3231ff849d14bf43c41b55b305a78a
bogdan <39177903+bn784@users.noreply.github.com>
2023-02-01 04:21:14 +0200

    UserSeeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $items = [
+
+            ['id' => 1, 'name' => 'administrator', 'email' => 'administrator@example.com', 'password' => bcrypt('12345678'),
+             'role_id' => 1, 'remember_token' => '',],
+
+            ['id' => 2, 'name' => 'user', 'email' => 'user@example.com', 'password' => bcrypt('12345678'), 'role_id' =>2,
+             'remember_token' => '',],
+
+        ];
+
+        foreach ($items as $item) {
+            \App\User::create($item);
+        }
+    }
+}
